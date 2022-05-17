155895ba92cea6feda82869283e9177193b973df
lrobt97 <lukerthomson@gmail.com>
2022-05-17 18:35:31 +0100

    Balancing fixes: - Th, Tc are now fixed. Tmax is removed. - c1 nerfed to now scale by 1.5

--- Control Theory.js
+++ Control Theory.js
@@ -8,16 +8,15 @@ var id = "control_theory";
 var name = "Control Theory";
 var description = "Control Theory is a tool used in engineering to maintain a variable at a set value (known as the 'set point'). \n \n To make progress, you will need to disturb T to change rho, however going over a certain threshold will reset your progress. You will also need to grow the variable 'r', this grows faster when T is close to the setpoint, T_sp. \n \n The controller works by calculating the error, e(t) between T and the set point, T_sp. The controller used in this theory will be a PID -- proportional, integral and derivative controller. K_p represents the proportional gain of the system - in other words how much the output changes depending on the error sum within the brackets. The integral term sums up the past errors and attempts to minimise the error after t_i seconds. The derivative term attempts to predict the future error after t_d seconds based on the current derivative of e(t). At some point you will also be able to manually change the values k_p, t_i, t_d, and T_sp to explore the system more deeply and to improve rho gain.\n \n For this example, you will assume that this is a heating controller system. The PID controller will adjust the heater so that T reaches the set point. For the purpose of the simulation, u(t) will be considered as a percentage change, in the real world this would correspond to opening a valve to allow heating/cooling fluid to change the temperature. \n \n "; 
 var authors = "Gaunter#7599, peanut#6368";
-var version = "1.3.3";
+var version = "1.3.4";
 var publicationExponent = 0.1;
 var achievements;
 requiresGameVersion("1.4.29");
-
 // Currency
 var rho;
 
 // System variables
-var d1, d0, fd1, fd0, r, T, output, kp, td, ti, setPoint, output, error, integral, systemDt, valve, timer, amplitude, frequency, autoKickerEnabled, baseTolerance, achievementMultiplier, publicationCount;
+var Tc, Th, d1, d0, fd1, fd0, r, T, output, kp, td, ti, setPoint, output, error, integral, systemDt, valve, timer, amplitude, frequency, autoKickerEnabled, baseTolerance, achievementMultiplier, publicationCount;
 
 var initialiseSystem = () => {
 timer = 0;
@@ -35,6 +34,8 @@ d1 = 0;
 d0 = 0;
 fd1 = 0;
 fd0 = 0;
+Tc = 30
+Th = 200
 amplitude = 125;
 autoKickerEnabled = false;
 baseTolerance = 5;
@@ -43,7 +44,7 @@ publicationCount = 0;
 
 }
 // Upgrades
-var c1, Th, Tc, r1, r2, kickT, Tmax, changePidValues, autoKick, achievementMultiplierUpgrade, tDotExponent;
+var c1, r1, r2, kickT, changePidValues, autoKick, achievementMultiplierUpgrade, tDotExponent;
 
 // Milestones
 var c1Exponent, rExponent, toleranceReduction;
@@ -125,7 +126,7 @@ var init = () => {
 
   // c1
   {
-    let getDesc = (level) => "c_1=2^{" + level + "}";
+    let getDesc = (level) => "c_1= 1.5^{" + level + "}";
     let getInfo = (level) => "c_1=" + getC1(level).toString(0);
     c1 = theory.createUpgrade(1, rho, new ExponentialCost(3000, Math.log2(8)));
     c1.getDescription = (_) => Utils.getMath(getDesc(c1.level));
@@ -148,36 +149,6 @@ var init = () => {
     r2 = theory.createUpgrade(3, rho, new ExponentialCost(15, Math.log2(8)));
     r2.getDescription = (_) => Utils.getMath(getDesc(r2.level));
     r2.getInfo = (amount) => Utils.getMathTo(getInfo(r2.level), getInfo(r2.level + amount));
-  }
-
-  //Th
-  {
-    let getInfo = (level) => "T_h=" + getTh(level).toString();
-    let getDesc = (level) => "T_h=" + getTh(level).toString();
-    Th = theory.createUpgrade(4, rho, new ExponentialCost(3000, Math.log2(10)));
-    Th.maxLevel = 30;
-    Th.getDescription = (_) => Utils.getMath(getDesc(Th.level));
-    Th.getInfo = (amount) => Utils.getMathTo(getInfo(Th.level), getInfo(Th.level + amount))
-  }
-
-  //Tc
-  {
-    let getInfo = (level) => "T_c=" + getTc(level).toPrecision(3).toString();
-    let getDesc = (level) => "T_c=" + getTc(level).toPrecision(3).toString();
-    Tc = theory.createUpgrade(5, rho, new ExponentialCost(3000, Math.log2(10)));
-    Tc.maxLevel = 37;
-    Tc.getDescription = (_) => Utils.getMath(getDesc(Tc.level));
-    Tc.getInfo = (amount) => Utils.getMathTo(getInfo(Tc.level), getInfo(Tc.level + amount))
-  }
-
-  //Tmax
-  {
-    let getInfo = (level) => "T_{max}=" + getTmax(level).toString();
-    let getDesc = (level) => "T_{max}=" + getTmax(level).toString();
-    Tmax = theory.createUpgrade(6, rho, new ExponentialCost(10000, Math.log2(10)));
-    Tmax.getDescription = (_) => Utils.getMath(getDesc(Tmax.level));
-    Tmax.getInfo = (amount) => Utils.getMathTo(getInfo(Tmax.level), getInfo(Tmax.level + amount))
-    Tmax.bought = (_) => theory.invalidateTertiaryEquation();
   }
   //Tdot exponent
   {
@@ -307,8 +278,8 @@ var init = () => {
         ]
       })
     })
-    amplitudeSlider.maximum = getTmax(Tmax.level);         
-    amplitudeSlider.minimum = getTc(Tc.level);
+    amplitudeSlider.maximum = Th;         
+    amplitudeSlider.minimum = Tc;
     amplitudeSlider.value = amplitude; 
     frequencySlider.maximum = 60;
     frequencySlider.minimum = 1;
@@ -367,8 +338,8 @@ var init = () => {
         ]
       })
     })
-    setPointSlider.maximum = getTh(Th.level);
-    setPointSlider.minimum = getTc(Tc.level);
+    setPointSlider.maximum = Th;
+    setPointSlider.minimum = Tc;
     setPointSlider.value =  setPoint;
     return menu;
   }
@@ -388,9 +359,6 @@ var init = () => {
     r1.level = 0;
     r2.level = 0;
     tDotExponent.level = 0;
-    Th.level = 0;
-    Tc.level = 0;
-    Tmax.level = 0;
     rho.value=BigNumber.ZERO;
     initialiseSystem();
   }
@@ -400,55 +368,40 @@ var init = () => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
     if (achievementMultiplierUpgrade.level > 0) bonus *= achievementMultiplier;
-
     error[2] = error[1];
     error[1] = error[0];
-    error[0] = T - setPoint;
-    let A0 = kp * kp/ti * (dt);
+    error[0] = setPoint - T;
+    let A0 = kp +  (Math.abs(error[0]) <= getTolerance(toleranceReduction.level)) *kp/ti * (systemDt);
     let A1 = -1 * kp;
-    let A0d = kp*td/(dt);
-    let A1d = - 2 * kp*td/(dt);
-    let A2d = kp*td/(dt);
-    let N = 1;
+    let A0d = kp*td/(systemDt);
+    let A1d = - 2 * kp*td/(systemDt);
+    let A2d = kp*td/(systemDt);
+    let N = 5;
     let timeConstant = td/N;
-    let alpha = (dt)/(2*timeConstant);
-    output = A0 * error[0] + A1 * error[1];
+    let alpha = (systemDt)/(2*timeConstant);
+    output = output + A0 * error[0] + (Math.abs(error[0]) <= getTolerance(toleranceReduction.level)) * A1 * error[1];
     d1 = d0
     d0 = A0d * error[0] + A1d * error[1] + A2d * error[2];
     fd1 = fd0;
     fd0 = ((alpha)/(alpha+1)) * (d1 + d0) - ((alpha - 1)/(alpha + 1)) * fd1
-    output = (output + fd0);
-    if (Math.abs(error) <= getTolerance(toleranceReduction.level)) {
-      output = 0;
+    output = (output + (Math.abs(error[0]) <= getTolerance(toleranceReduction.level))*fd0);    
+    if (output>100){
+      valve = 1
     }
-    // Normalise output so it is proportional to the gap between setPoint and Tc/Th
-    if (output > 0) {
-      if (T + output  < getTh(Th.level)) {
-        valveTarget = ((T + output) - getTh(Th.level)) / (T - getTh(Th.level));
-      }
-      else{
-        valveTarget = 1
-      }
+    else if (output < -100)
+    {
+      valve = -1
     }
-    else if (output < 0){
-      if (T + output  > getTc(Tc.level)) {
-        valveTarget = -1* ((T + output) - getTc(Tc.level))/(T - getTc(Tc.level));
-      }
-      else{
-        valveTarget = -1
-      }    
+    else{
+      valve = output/100;
     }
-    else if (Math.abs(error[0]) < getTolerance(toleranceReduction.level)) valveTarget = 0;
 
     let dT = 0;
-
-    // Take the exponential moving average to smooth the transition
-    valve = 0.03 * valveTarget + 0.97 * valve;
     let prevT = T;
     if (valve > 0) {
-      T = getTh(Th.level) + (T - getTh(Th.level)) * BigNumber.E.pow(-1 * Math.abs(valve) * dt)
+      T = Th + (T - Th) * BigNumber.E.pow(-1 * Math.abs(valve) * dt)
     } else if (valve < 0) {
-      T = getTc(Tc.level) + (T - getTc(Tc.level)) * BigNumber.E.pow(-1 * Math.abs(valve) * dt)
+      T = Tc + (T - Tc) * BigNumber.E.pow(-1 * Math.abs(valve) * dt)
     }
 
     dT = BigNumber.from((T - prevT) / dt).abs();
@@ -456,7 +409,7 @@ var init = () => {
 
     let value_c1 = getC1(c1.level).pow(getC1Exp(c1Exponent.level));
     let value_r = r.pow(getRExp(rExponent.level))
-
+    log(dT);
     rho.value += value_r * BigNumber.from(value_c1 * dT.pow(getTdotExponent(tDotExponent.level))).sqrt() * dt * bonus; // use bignumber sqrt and pow, not Math ones, they dont support values above 1e308 - peanut
 
     // reset integral error when system converges
@@ -469,11 +422,6 @@ var init = () => {
       T = amplitude;
       timer = 0;
     }
-
-    if (T > getTmax(Tmax.level)) {
-      resetStage();
-    }
-
     theory.invalidateTertiaryEquation();
   }
 }
@@ -490,7 +438,7 @@ var init = () => {
     let c1_exp = c1Exponent.level > 0 ? getC1Exp(c1Exponent.level).toNumber() : "";
     let r1_exp = rExponent.level > 0 ? getRExp(rExponent.level).toNumber() : "";
 
-    result += "\\dot{T} = \\left\\{ \\begin{array}{cl} Q_{h} & : \\ u(t) > 0, \\ Q_h = T_h - T \\\\ Q_{c} & : \\ u(t) < 0, \\ Q_c = T-T_c  \\end{array} \\right.\\\\";
+    result += "\\dot{T} = \\left\\{ \\begin{array}{cl} Q_{h} & : \\ u(t) > 0, \\ Q_h = " + Th +" - T \\\\ Q_{c} & : \\ u(t) < 0, \\ Q_c = T- "+ Tc + "  \\end{array} \\right.\\\\";
 
     result += "\\dot{\\rho} = r^{" + r1_exp + "}\\sqrt{c_1^{" + c1_exp +"}\\dot{T}^{" + getTdotExponent(tDotExponent.level) + "}}";
     result += ", \\;\\dot{r} = \\frac{r_1 r_2}{1+\|e(t)\|}"
@@ -505,7 +453,7 @@ var init = () => {
     let result = "\\begin{array}{c}";
     result += "e(t) = T_{sp} - T \\\\";
     result += "u(t) = K_p(e(t) + \\frac{1}{t_i}\\int_{0}^{t}e(\\tau)d\\tau \\ + t_d \\dot{e(t)})\\\\";
-    result += theory.latexSymbol + "=\\max\\rho^{"+publicationExponent+"} , \\ K_p =" + kp.toPrecision(2) + ",\\ t_i =" + ti.toPrecision(2) + ",\\ t_d =" + td.toPrecision(2) + "\, \\ T_{max} =" + getTmax(Tmax.level);
+    result += theory.latexSymbol + "=\\max\\rho^{"+publicationExponent+"} , \\ K_p =" + kp.toPrecision(2) + ",\\ t_i =" + ti.toPrecision(2) + ",\\ t_d =" + td.toPrecision(2);
     result += "\\end{array}"
     return result;
   }
@@ -523,19 +471,16 @@ var init = () => {
 var getC1Exp = (level) => BigNumber.from(1 + c1Exponent.level * 0.05);
 var getRExp = (level) => BigNumber.from(1 + rExponent.level * 0.05);
 
-var getC1 = (level) => BigNumber.TWO.pow(level);
+var getC1 = (level) => BigNumber.from(1.5).pow(level);
 var getR1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
 var getR2 = (level) => BigNumber.TWO.pow(level);
-var getTh = (level) => 110 + 10 * level;
-var getTc = (level) => 96.9 - 10 * level;
-var getTmax = (level) => 150 + 10 * level;
 var getTolerance = (level) => parseFloat(baseTolerance * BigNumber.TEN.pow(-parseInt(level)));
 var getTdotExponent = (level) => 2 + level;
 var getPublicationMultiplier = (tau) => achievementMultiplier * tau;
 var getPublicationMultiplierFormula = (symbol) => (achievementMultiplier > 1 ? BigNumber.from(achievementMultiplier).toString(2) + "\\times" + symbol : symbol);
 var get2DGraphValue = () => (BigNumber.ONE + T).toNumber();
 var getTau = () => rho.value.pow(publicationExponent);
-var getCurrencyFromTau = (tau) => [tau.max(BigNumber.ONE).pow(3), rho.symbol];
+var getCurrencyFromTau = (tau) => [tau.max(BigNumber.ONE).pow(10), rho.symbol];
 var postPublish = () => {
   initialiseSystem();
   theory.invalidatePrimaryEquation();
